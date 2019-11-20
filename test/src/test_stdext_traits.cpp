35e028944bb929f5a933248ea0cd02580dce1412
Vahid Dzanic <vdc@gmx.ch>
2019-11-21 00:11:32 +0100

    Improve license visibility and imporve gtest lib dependency

--- test/src/test_stdext_traits.cpp
+++ test/src/test_stdext_traits.cpp
@@ -1,6 +1,30 @@
 #include <gtest/gtest.h>
 #include <stdex/std_type_sets.h>
 
+// This file is part of stdex library, stdex is an extension of the c++-standard library,
+// especially the type traits for standard containers.
+//
+// Copyright (C) 2019 Vahid Dzanic <vdc@gmx.ch>
+//
+// stdex is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 3 of the License, or (at your option) any later version.
+//
+// Alternatively, you can redistribute it and/or
+// modify it under the terms of the GNU General Public License as
+// published by the Free Software Foundation; either version 2 of
+// the License, or (at your option) any later version.
+//
+// stdex is distributed in the hope that it will be useful, but WITHOUT ANY
+// WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+// FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License or the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License and a copy of the GNU General Public License along with
+// stdex. If not, see <http://www.gnu.org/licenses/>.
+
 template<typename _Tp>
 class test_fixture_raw : public ::testing::Test
 {};
